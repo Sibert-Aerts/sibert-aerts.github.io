d802b26080ebbd3b6e79fb2f685cdb2a8c633f45
Sibert Aerts <sibert.aerts@gmail.com>
2022-05-14 18:27:51 +0200

    Half of a refactor

--- new-area/image-gen.js
+++ new-area/image-gen.js
@@ -4,6 +4,7 @@
 
 //// UTIL FUNCTIONS
 const byId = id => document.getElementById(id)
+const makeElem = (tag, clss, text) => { let e = document.createElement(tag); if(clss) e.className = clss; if(text) e.textContent = text; return e }
 const getInput = name => document.getElementsByTagName('input').namedItem(name)
 
 
@@ -197,216 +198,13 @@ function redrawImage() {
         ctx.drawImage(selectedImage, 0, 0)
         ctx.filter = 'none'
     }
+    // UI changes
     byId('resolution-warning').hidden = !canvasTooBig()
     dimView.x.textContent = canvas.width
     dimView.y.textContent = canvas.height
     
 
-    if( macroTypeSelect.value === 'ds3-area' )
-        drawDS3AreaName()
-    else if ( macroTypeSelect.value === 'ds3-death' )
-        drawDS3Death()
-    else if ( macroTypeSelect.value === 'ds1-victory' )
-        drawDS1Victory()
-}
-
-
-
-/** Called by redrawImage() */
-function drawDS3AreaName() {
-    resetDrawingState()
-
-    // Prefer title-case caption
-    if( captionInput.value === 'ENTER CAPTION' )
-        captionInput.value = 'Enter Caption'
-
-    // CONSTANTS
-    const w = canvas.width, h = canvas.height
-    let s = h/1080
-
-    // USER INPUT
-    const x0 = parseFloat(x0Input.value)/100 * w
-    const y0 = parseFloat(y0Input.value)/100 * h
-    const s0 = 2**parseFloat(scaleInput.value)
-    ctx.translate(x0, y0)
-    s *= s0
-    const underline = parseFloat(underlineInput.value)/100
-    let contrast = parseFloat(contrastInput.value)
-
-    // UNDERLINE
-    if( underline > 0 ) {
-        const left = (.5-underline)*w, right = (.5+underline)*w
-        const length = 2*underline*w
-
-        const shadowGrad = ctx.createLinearGradient(left, 0, right, 0)
-        shadowGrad.addColorStop(0,   '#0000')
-        shadowGrad.addColorStop(0.1, `rgba(0, 0, 0, ${.25 * contrast})`)
-        shadowGrad.addColorStop(0.9, `rgba(0, 0, 0, ${.25 * contrast})`)
-        shadowGrad.addColorStop(1,   '#0000')
-        ctx.fillStyle = shadowGrad
-        ctx.fillRect(left, .51*h+5*s, length, 3*s)
-
-        const grad = ctx.createLinearGradient(left, 0, right, 0)
-        grad.addColorStop(0,   '#fff0')
-        grad.addColorStop(0.1, `rgba(255, 255, 255, ${.75 * Math.max(1, contrast)})`)
-        grad.addColorStop(0.9, `rgba(255, 255, 255, ${.75 * Math.max(1, contrast)})`)
-        grad.addColorStop(1,   '#fff0')
-        ctx.fillStyle = grad
-        ctx.fillRect(left, .51*h, length, 5*s)
-    }
-
-    // TEXT
-    ctx.shadowOffsetX = 2*s
-    ctx.shadowOffsetY = 1*s
-    ctx.shadowBlur = 8*s
-
-    ctx.font = Math.floor(96*s) + 'px adobe-garamond-pro'
-    ctx.fillStyle = 'white'
-    ctx.textAlign = 'center'
-
-    // Apply contrast by just redrawing the same text so the shadow overlaps
-    //      0.85 * 1.17 ~= 1
-    while( contrast >= 0 ) {
-        ctx.shadowColor = `rgba(0, 0, 0, ${.85 * Math.min(contrast, 1.17)})`
-        ctx.fillText(captionInput.value, w/2, h*(0.5 + (1-(s0-1)/3)*0.007 ))
-        contrast -= 1.17
-    }
-}
-
-/** Called by redrawImage() */
-function drawDS3Death() {
-    resetDrawingState()
-
-    // Prefer all-caps caption
-    if( captionInput.value === 'Enter Caption' )
-        captionInput.value = 'ENTER CAPTION'
-
-    // CONSTANTS
-    const w = canvas.width, h = canvas.height
-    let s = h/1080
-
-    // USER INPUT
-    const x0 = (parseFloat(x0Input.value) + .6)/100 * w
-    const y0 = (parseFloat(y0Input.value) + 1.8)/100 * h
-    const s0 = 2**parseFloat(scaleInput.value)
-    s *= s0
-    const shadeScale = parseFloat(underlineInput.value)/32 * s0
-    let contrast = parseFloat(contrastInput.value)
-
-    // The shade only moves up or down
-    ctx.translate(0, y0)
-    // SHADE
-    if( shadeScale > 0 ) {
-        const shadeHeight = shadeScale * .2*h
-        const top = .5*h-shadeHeight/2, bottom = .5*h+shadeHeight/2
-
-        const shadowGrad = ctx.createLinearGradient(0, top, 0, bottom)
-        shadowGrad.addColorStop(0,   '#0000')
-        shadowGrad.addColorStop(0.25, `rgba(0, 0, 0, ${.5 * contrast**0.7})`)
-        shadowGrad.addColorStop(0.75, `rgba(0, 0, 0, ${.5 * contrast**0.7})`)
-        shadowGrad.addColorStop(1,   '#0000')
-        ctx.fillStyle = shadowGrad
-        ctx.fillRect(0, top, w, shadeHeight)
-    }
-    
-    // The text also moves left or right
-    ctx.translate(x0, 0)
-
-    // TEXT
-    ctx.font = Math.floor(148*s) + 'px adobe-garamond-pro'
-    ctx.fillStyle = `rgb(${120*contrast}, ${10*contrast}, ${20*contrast})`
-    ctx.textAlign = 'center'
-    
-    ctx.shadowBlur = 4*s
-    ctx.shadowColor = `rgba(255, 20, 20, .2)`
-
-    ctx.scale(1, 1.3)
-    ctx.fillText(captionInput.value, w/2, (h/2 + 50*s)/1.3 )
-}
-
-/** Called by redrawImage() */
-function drawDS1Victory() {
-    resetDrawingState()
-
-    // Prefer all-caps caption
-    if( captionInput.value === 'Enter Caption' )
-        captionInput.value = 'ENTER CAPTION'
-
-    // CONSTANTS
-    const w = canvas.width, h = canvas.height
-    let s = h/1080
-
-    // USER INPUT
-    const x0 = (parseFloat(x0Input.value) + .4)/100 * w
-    const y0 = (parseFloat(y0Input.value))/100 * h
-    const s0 = 2**parseFloat(scaleInput.value)
-    s *= s0
-    const shadeScale = parseFloat(underlineInput.value)/32 * s0
-    let contrast = parseFloat(contrastInput.value)
-
-    // The shade only moves up or down
-    ctx.translate(0, y0)
-    // SHADE
-    if( shadeScale > 0 ) {
-        const shadeHeight = shadeScale * .25*h
-        const SHADECENTER = .525*h
-        const top = SHADECENTER-shadeHeight/2, bottom = SHADECENTER+shadeHeight/2
-
-        const shadowGrad = ctx.createLinearGradient(0, top, 0, bottom)
-        shadowGrad.addColorStop(0,   '#0000')
-        shadowGrad.addColorStop(0.25, `rgba(0, 0, 0, ${.7 * contrast**0.4})`)
-        shadowGrad.addColorStop(0.75, `rgba(0, 0, 0, ${.7 * contrast**0.4})`)
-        shadowGrad.addColorStop(1,   '#0000')
-        ctx.fillStyle = shadowGrad
-        ctx.fillRect(0, top, w, shadeHeight)
-    }
-    
-    // The text also moves left or right
-    ctx.translate(x0, 0)
-
-    // TEXT
-    caption = captionInput.value
-
-    // This feature only works on chromia, otherwise just inject hair-spaces between letters
-    if( !!window.chrome )
-        canvas.style.letterSpacing = Math.floor(8*s) + 'px'
-    else
-        caption = caption.split('').join(' ')
-
-    ctx.font = Math.floor(92*s) + 'px adobe-garamond-pro'
-    ctx.textAlign = 'center'
-    const MAGICOFFSET = 69*s
-    ctx.translate(0, MAGICOFFSET)
-    
-    const VSCALE = 1.5
-    ctx.scale(1, VSCALE)
-        
-    // Emulate the zoom blur effect
-    const zoomSteps = Math.floor(20 * Math.pow(s, 1/4))
-    const ZOOMSIZE = 1.10
-    // zoomFactor**zoomSteps = ZOOMSIZE
-    const zoomFactor = Math.pow(ZOOMSIZE, 1/zoomSteps)
-    // Zoom blur center offset
-    const VOFFSET = MAGICOFFSET
-
-    for( let i=0; i<=zoomSteps; i++ ) {
-        if( i ) ctx.scale(zoomFactor, zoomFactor)
-        // `product` ranges from 1 up to and including ZOOMSIZE
-        const product = Math.pow(ZOOMSIZE, i/zoomSteps)
-        // `fatProduct` ranges from 1 up to and including ±2
-        const fatProduct = product ** 7
-
-        ctx.filter = `blur(${Math.floor(s*fatProduct)}px`
-        ctx.fillStyle = `rgba(255, 180, 60, ${0.1 / fatProduct})`
-        ctx.fillText(caption, w/2/product, ((h/2-VOFFSET)/product+VOFFSET)/VSCALE)
-    }
-
-    resetDrawingState()
-    ctx.filter = 'none'
-    ctx.translate(x0, y0 + MAGICOFFSET)
-    ctx.scale(1, VSCALE)
-    ctx.fillStyle = `rgba(255, 255, 107, 0.9)`
-    ctx.fillText(caption, w/2, h/2/VSCALE )
+    layerTypes[macroTypeSelect.value].draw()
 }
 
 /** Save the current contents of the canvas to the user's computer. */
@@ -424,3 +222,269 @@ function saveImage() {
 
     saveLink.click()
 }
+
+
+window.addEventListener('load', function() {
+    if( macroTypeSelect ) {
+        for( const layerType of layerTypeList ) {
+            const elem = makeElem('option', null, layerType.name)
+            elem.value = layerType.key
+            macroTypeSelect.appendChild(elem)
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////// NEO
+
+
+
+/** 
+ * @typedef {Object} DrawableLayer
+ * @prop {string} key
+ * @prop {string} name
+ * @prop {() => void} draw()
+ */
+
+
+/** @type DrawableLayer */
+const ds1Victory = {
+    key: 'ds1-victory',
+    name: 'DS1 - Victory',
+
+    draw() {
+        resetDrawingState()
+    
+        // Prefer all-caps caption
+        if( captionInput.value === 'Enter Caption' )
+            captionInput.value = 'ENTER CAPTION'
+    
+        // CONSTANTS
+        const w = canvas.width, h = canvas.height
+        let s = h/1080
+    
+        // USER INPUT
+        const x0 = (parseFloat(x0Input.value) + .4)/100 * w
+        const y0 = (parseFloat(y0Input.value))/100 * h
+        const s0 = 2**parseFloat(scaleInput.value)
+        s *= s0
+        const shadeScale = parseFloat(underlineInput.value)/32 * s0
+        let contrast = parseFloat(contrastInput.value)
+    
+        // The shade only moves up or down
+        ctx.translate(0, y0)
+        // SHADE
+        if( shadeScale > 0 ) {
+            const shadeHeight = shadeScale * .25*h
+            const SHADECENTER = .525*h
+            const top = SHADECENTER-shadeHeight/2, bottom = SHADECENTER+shadeHeight/2
+    
+            const shadowGrad = ctx.createLinearGradient(0, top, 0, bottom)
+            shadowGrad.addColorStop(0,   '#0000')
+            shadowGrad.addColorStop(0.25, `rgba(0, 0, 0, ${.7 * contrast**0.4})`)
+            shadowGrad.addColorStop(0.75, `rgba(0, 0, 0, ${.7 * contrast**0.4})`)
+            shadowGrad.addColorStop(1,   '#0000')
+            ctx.fillStyle = shadowGrad
+            ctx.fillRect(0, top, w, shadeHeight)
+        }
+        
+        // The text also moves left or right
+        ctx.translate(x0, 0)
+    
+        // TEXT
+        caption = captionInput.value
+    
+        // This feature only works on chromia, otherwise just inject hair-spaces between letters
+        if( !!window.chrome )
+            canvas.style.letterSpacing = Math.floor(8*s) + 'px'
+        else
+            caption = caption.split('').join(' ')
+    
+        ctx.font = Math.floor(92*s) + 'px adobe-garamond-pro'
+        ctx.textAlign = 'center'
+        const MAGICOFFSET = 69*s
+        ctx.translate(0, MAGICOFFSET)
+        
+        const VSCALE = 1.5
+        ctx.scale(1, VSCALE)
+            
+        // Emulate the zoom blur effect
+        const zoomSteps = Math.floor(20 * Math.pow(s, 1/4))
+        const ZOOMSIZE = 1.10
+        // zoomFactor**zoomSteps = ZOOMSIZE
+        const zoomFactor = Math.pow(ZOOMSIZE, 1/zoomSteps)
+        // Zoom blur center offset
+        const VOFFSET = MAGICOFFSET
+    
+        for( let i=0; i<=zoomSteps; i++ ) {
+            if( i ) ctx.scale(zoomFactor, zoomFactor)
+            // `product` ranges from 1 up to and including ZOOMSIZE
+            const product = Math.pow(ZOOMSIZE, i/zoomSteps)
+            // `fatProduct` ranges from 1 up to and including ±2
+            const fatProduct = product ** 7
+    
+            ctx.filter = `blur(${Math.floor(s*fatProduct)}px`
+            ctx.fillStyle = `rgba(255, 180, 60, ${0.1 / fatProduct})`
+            ctx.fillText(caption, w/2/product, ((h/2-VOFFSET)/product+VOFFSET)/VSCALE)
+        }
+    
+        resetDrawingState()
+        ctx.filter = 'none'
+        ctx.translate(x0, y0 + MAGICOFFSET)
+        ctx.scale(1, VSCALE)
+        ctx.fillStyle = `rgba(255, 255, 107, 0.9)`
+        ctx.fillText(caption, w/2, h/2/VSCALE )
+    }
+}
+
+/** @type DrawableLayer */
+const ds3Death = {
+    key: 'ds3-death',
+    name: 'DS3 - Death',
+
+    draw() {
+        resetDrawingState()
+
+        // Prefer all-caps caption
+        if( captionInput.value === 'Enter Caption' )
+            captionInput.value = 'ENTER CAPTION'
+
+        // CONSTANTS
+        const w = canvas.width, h = canvas.height
+        let s = h/1080
+
+        // USER INPUT
+        const x0 = (parseFloat(x0Input.value) + .6)/100 * w
+        const y0 = (parseFloat(y0Input.value) + 1.8)/100 * h
+        const s0 = 2**parseFloat(scaleInput.value)
+        s *= s0
+        const shadeScale = parseFloat(underlineInput.value)/32 * s0
+        let contrast = parseFloat(contrastInput.value)
+
+        // The shade only moves up or down
+        ctx.translate(0, y0)
+        // SHADE
+        if( shadeScale > 0 ) {
+            const shadeHeight = shadeScale * .2*h
+            const top = .5*h-shadeHeight/2, bottom = .5*h+shadeHeight/2
+
+            const shadowGrad = ctx.createLinearGradient(0, top, 0, bottom)
+            shadowGrad.addColorStop(0,   '#0000')
+            shadowGrad.addColorStop(0.25, `rgba(0, 0, 0, ${.5 * contrast**0.7})`)
+            shadowGrad.addColorStop(0.75, `rgba(0, 0, 0, ${.5 * contrast**0.7})`)
+            shadowGrad.addColorStop(1,   '#0000')
+            ctx.fillStyle = shadowGrad
+            ctx.fillRect(0, top, w, shadeHeight)
+        }
+        
+        // The text also moves left or right
+        ctx.translate(x0, 0)
+
+        // TEXT
+        ctx.font = Math.floor(148*s) + 'px adobe-garamond-pro'
+        ctx.fillStyle = `rgb(${120*contrast}, ${10*contrast}, ${20*contrast})`
+        ctx.textAlign = 'center'
+        
+        ctx.shadowBlur = 4*s
+        ctx.shadowColor = `rgba(255, 20, 20, .2)`
+
+        ctx.scale(1, 1.3)
+        ctx.fillText(captionInput.value, w/2, (h/2 + 50*s)/1.3 )
+    }
+}
+
+/** @type DrawableLayer */
+const ds3Area = {
+    key: 'ds3-area',
+    name: 'DS3 - Area Name',
+
+    draw() {
+        resetDrawingState()
+    
+        // Prefer title-case caption
+        if( captionInput.value === 'ENTER CAPTION' )
+            captionInput.value = 'Enter Caption'
+    
+        // CONSTANTS
+        const w = canvas.width, h = canvas.height
+        let s = h/1080
+    
+        // USER INPUT
+        const x0 = parseFloat(x0Input.value)/100 * w
+        const y0 = parseFloat(y0Input.value)/100 * h
+        const s0 = 2**parseFloat(scaleInput.value)
+        ctx.translate(x0, y0)
+        s *= s0
+        const underline = parseFloat(underlineInput.value)/100
+        let contrast = parseFloat(contrastInput.value)
+    
+        // UNDERLINE
+        if( underline > 0 ) {
+            const left = (.5-underline)*w, right = (.5+underline)*w
+            const length = 2*underline*w
+    
+            const shadowGrad = ctx.createLinearGradient(left, 0, right, 0)
+            shadowGrad.addColorStop(0,   '#0000')
+            shadowGrad.addColorStop(0.1, `rgba(0, 0, 0, ${.25 * contrast})`)
+            shadowGrad.addColorStop(0.9, `rgba(0, 0, 0, ${.25 * contrast})`)
+            shadowGrad.addColorStop(1,   '#0000')
+            ctx.fillStyle = shadowGrad
+            ctx.fillRect(left, .51*h+5*s, length, 3*s)
+    
+            const grad = ctx.createLinearGradient(left, 0, right, 0)
+            grad.addColorStop(0,   '#fff0')
+            grad.addColorStop(0.1, `rgba(255, 255, 255, ${.75 * Math.max(1, contrast)})`)
+            grad.addColorStop(0.9, `rgba(255, 255, 255, ${.75 * Math.max(1, contrast)})`)
+            grad.addColorStop(1,   '#fff0')
+            ctx.fillStyle = grad
+            ctx.fillRect(left, .51*h, length, 5*s)
+        }
+    
+        // TEXT
+        ctx.shadowOffsetX = 2*s
+        ctx.shadowOffsetY = 1*s
+        ctx.shadowBlur = 8*s
+    
+        ctx.font = Math.floor(96*s) + 'px adobe-garamond-pro'
+        ctx.fillStyle = 'white'
+        ctx.textAlign = 'center'
+    
+        // Apply contrast by just redrawing the same text so the shadow overlaps
+        //      0.85 * 1.17 ~= 1
+        while( contrast >= 0 ) {
+            ctx.shadowColor = `rgba(0, 0, 0, ${.85 * Math.min(contrast, 1.17)})`
+            ctx.fillText(captionInput.value, w/2, h*(0.5 + (1-(s0-1)/3)*0.007 ))
+            contrast -= 1.17
+        }
+    }
+}
+
+const emptyLayer = {
+    key: 'none',
+    name: 'Nothing',
+    draw() {}
+}
+
+const layerTypeList = [ds1Victory, ds3Area, ds3Death, emptyLayer]
+
+/** 
+ * Object containing all types of drawable layers, indexed by key.
+ * @type {{[key: string] : DrawableLayer}} 
+ */
+const layerTypes = {}
+layerTypeList.map( macro => layerTypes[macro.key] = macro)
