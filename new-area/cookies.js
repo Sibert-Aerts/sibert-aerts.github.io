e8d6589596a89516f038471553a997e41a0cf25f
Sibert Aerts <sibert.aerts@gmail.com>
2022-06-07 17:39:39 +0200

    I didn't know good how cookies worked

--- new-area/cookies.js
+++ new-area/cookies.js
@@ -1,40 +1,48 @@
 
-// Important keys
-const id = 'G-HCGGJHJ3DR'
-const disableKey = 'ga-disable-' + id
-
-// Collect our tracking configuration cookies
-let hideBanner = (document.cookie.indexOf('hide-banner') >= 0)
-let disabled = (document.cookie.indexOf('ga-disable=1') >= 0)
-
-// Set up GTagOptIn to allow us to opt in or opt out later
-GTagOptIn.register(id)
-
-// If the user has already hidden the banner AND enabled tracking, then go!
-if( hideBanner ) {
-    if( disabled ) GTagOptIn.optOut()
-    else GTagOptIn.optIn()
+function setCookie(name, value='', exdays=365) {
+    const d = new Date()
+    d.setTime(d.getTime() + (exdays*24*60*60*1000))
+    document.cookie = `${name}=${value}; expires=${d.toUTCString()}; path=/`
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+{
+    // Important keys
+    const id = 'G-HCGGJHJ3DR'
+    const disableKey = 'ga-disable-' + id
 
-    // Set up the 'Enable Google Analytics' checkboxes
-    document.querySelectorAll('input[target=enable-ga]').forEach(e => {
-        e.checked = hideBanner && !disabled
-        e.onclick = function() {
-            // Opt in or out depending
-            if( this.checked ) GTagOptIn.optOut()
-            else GTagOptIn.optIn()
-            // Set the cookie
-            document.cookie = 'ga-disable='+ +!this.checked
-        }
-    })    
+    // Read the tracking configuration cookies
+    let hideBanner = (document.cookie.indexOf('hide-banner') >= 0)
+    let disabled = (document.cookie.indexOf('ga-disable=1') >= 0)
 
-    // Set up the cookies checkboxes
-    if( hideBanner ) removeCookieBanner()
-    else showCookieBanner()
+    // Set up GTagOptIn to allow us to opt in or opt out later
+    GTagOptIn.register(id)
 
-})
+    // If the user has already hidden the banner AND enabled tracking, then go!
+    if( hideBanner ) {
+        if( disabled ) GTagOptIn.optOut()
+        else GTagOptIn.optIn()
+    }
+    
+    document.addEventListener('DOMContentLoaded', function() {
+
+        // Set up the 'Enable Google Analytics' checkboxes
+        document.querySelectorAll('input[target=enable-ga]').forEach(e => {
+            e.checked = hideBanner && !disabled
+            e.onclick = function() {
+                // Opt in or out depending
+                if( this.checked ) GTagOptIn.optOut()
+                else GTagOptIn.optIn()
+                // Set the cookie
+                setCookie('ga-disable', this.checked? '0': '1')
+            }
+        })    
+
+        // Set up the cookies checkboxes
+        if( hideBanner ) removeCookieBanner()
+        else showCookieBanner()
+
+    })
+}
 
 function showCookieBanner() {
     Array.from(document.getElementsByClassName('cookie-banner')).forEach(e => e.classList.remove('hidden'))
@@ -44,16 +52,16 @@ function removeCookieBanner() {
 }
 
 function consentToTracking() {
-    document.cookie = 'hide-banner'
-    document.cookie = 'ga-disable=0'
+    setCookie('hide-banner')
+    setCookie('ga-disable', 0)
     GTagOptIn.optIn()
     removeCookieBanner()
     document.querySelectorAll('input[target=enable-ga]').forEach(e => e.checked = true)
 }
 
 function denyTracking() {
-    document.cookie = 'hide-banner'
-    document.cookie = 'ga-disable=1'
+    setCookie('hide-banner')
+    setCookie('ga-disable', 1)
     GTagOptIn.optOut()
     removeCookieBanner()
     document.querySelectorAll('input[target=enable-ga]').forEach(e => e.checked = false)
